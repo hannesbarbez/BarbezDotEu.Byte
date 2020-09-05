4ef8a3c67b81a94fd86698b56e1c93205c43e355
Hannes Barbez <hannesbarbez@hotmail.com>
2020-09-05 12:38:44 +0200

    Initial commit;

---
+++ BarbezDotEu.Byte/BarbezDotEu.Byte/ByteHelper.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Text;
+
+namespace BarbezDotEu.Byte
+{
+    /// <summary>
+    /// Helper class containing diverse static methods that can be used in any type of context.
+    /// </summary>
+    public static class ByteHelper
+    {
+        /// <summary>
+        /// Creates a byte array from the string, using the 
+        /// System.Text.Encoding.Default encoding unless another is specified.
+        /// </summary>
+        public static byte[] ToByteArray(this string input, Encoding encoding = default)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return new byte[] { };
+
+            if (encoding == default)
+                encoding = Encoding.UTF8;
+
+            return encoding.GetBytes(input);
+        }
+    }
+}
