ff6c400ece7533a74eccba310e1a8fcf4ee3b56c
Hannes Barbez <hannesbarbez@hotmail.com>
2022-05-07 00:30:23 +0200

    Extended ByteHelper class

--- BarbezDotEu.Byte/BarbezDotEu.Byte/ByteHelper.cs
+++ BarbezDotEu.Byte/BarbezDotEu.Byte/ByteHelper.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Hannes Barbez. All rights reserved.
 // Licensed under the GNU General Public License v3.0
 
+using System;
+using System.Linq;
 using System.Text;
 
 namespace BarbezDotEu.Byte
@@ -23,6 +25,36 @@ namespace BarbezDotEu.Byte
                 encoding = Encoding.UTF8;
 
             return encoding.GetBytes(input);
+        }
+
+        /// <summary>
+        /// Converts a "written" byte-array to an actual byte array.
+        /// e.g. input string: 69CC766AEFAE05F6BE92A529E27D6AC50A0DC73EFB3631534B5E41A9311D56AB.
+        /// </summary>
+        /// <param name="hex">The "written" byte-array to convert.</param>
+        /// <returns>The actual byte-array representation.</returns>
+        /// <remarks>Based on https://stackoverflow.com/a/321404/8669939</remarks>
+        public static byte[] StringToByteArray(this string hex)
+        {
+            // TODO: Check what can and cannot be merged with ToByteArray method above, and rename or remove accordingly if needed.
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        /// <summary>
+        /// Generates a string representation of a byte array.
+        /// </summary>
+        /// <param name="hash">The hash to write as string.</param>
+        /// <returns>A string representation of the hash.</returns>
+        public static string ByteArrayToString(byte[] hash)
+        {
+            var stringBuilder = new StringBuilder(hash.Length * 2);
+            foreach (byte @byte in hash)
+                stringBuilder.Append(@byte.ToString("X2"));
+
+            return stringBuilder.ToString();
         }
     }
 }
