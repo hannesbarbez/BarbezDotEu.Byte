609ea1909b4032ab84a11ce5d5751efae0dc1323
Hannes Barbez <hannesbarbez@hotmail.com>
2023-02-03 17:21:45 +0100

    CryptoHelper.GetTsqlLikeHashBytes

---
+++ BarbezDotEu.Byte/BarbezDotEu.Byte/CryptoHelper.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BarbezDotEu.Byte
+{
+    /// <summary>
+    /// Helpers for cryptography.
+    /// </summary>
+    public static class CryptoHelper
+    {
+        /// <summary>
+        /// Equivalent of T-SQL's HASHBYTES('SHA2_256', &lt;toHash&gt;)".
+        /// </summary>
+        /// <param name="toHash">The string to hash.</param>
+        /// <returns>The result of the hash.</returns>
+        public static byte[] GetTsqlLikeHashBytes(string toHash)
+        {
+            byte[] hashable = Encoding.Unicode.GetBytes(toHash);
+            var hasher = SHA256.Create();
+            return hasher.ComputeHash(hashable);
+        }
+    }
+}
