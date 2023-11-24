ff98492b78eeaf59b4ab7df91c2e8430de54d4c9
Hannes Barbez <hannesbarbez@hotmail.com>
2023-11-24 21:40:14 +0100

    Replace TSQL reference with the more generic ComputeSha256Hash method

---
+++ BarbezDotEu.Byte/BarbezDotEu.Byte.Testing.Unit/CryptoHelperCan.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarbezDotEu.Byte.Testing.Unit
+{
+    [TestClass]
+    public class CryptoHelperCan
+    {
+        [TestMethod]
+        public void ProduceResultConsistentWithByteHelper()
+        {
+            var original = "Hannes";
+            var expectedSqlLikeHashBytes = CryptoHelper.ComputeSha256Hash(original, Encoding.UTF8);
+
+            var hashed = "388E3CC1559EAF11049F3E65223E823CF441F0D3B0C1B6BE2E41584C3A786945";
+            var output = ByteHelper.StringToByteArray(hashed);
+            Assert.IsTrue(Enumerable.SequenceEqual(output, expectedSqlLikeHashBytes));
+        }
+    }
+}
--- BarbezDotEu.Byte/BarbezDotEu.Byte/CryptoHelper.cs
+++ BarbezDotEu.Byte/BarbezDotEu.Byte/CryptoHelper.cs
@@ -12,13 +12,18 @@ namespace BarbezDotEu.Byte
     public static class CryptoHelper
     {
         /// <summary>
-        /// Equivalent of T-SQL's HASHBYTES('SHA2_256', &lt;toHash&gt;)".
+        /// Computes a hash using <see cref="SHA256"/> and a given <see cref="Encoding"/>.
         /// </summary>
         /// <param name="toHash">The string to hash.</param>
+        /// <param name="encoding">The encoding to use.</param>
         /// <returns>The result of the hash.</returns>
-        public static byte[] GetTsqlLikeHashBytes(string toHash)
+        /// <remarks>
+        /// As an aside, this is also the equivalent of T-SQL's HASHBYTES('SHA2_256', &lt;toHash&gt;)".
+        /// In that case, <paramref name="encoding"/> could correspond to your SQL Server collation.
+        /// </remarks>
+        public static byte[] ComputeSha256Hash(string toHash, Encoding encoding)
         {
-            byte[] hashable = Encoding.Unicode.GetBytes(toHash);
+            byte[] hashable = encoding.GetBytes(toHash);
             var hasher = SHA256.Create();
             return hasher.ComputeHash(hashable);
         }
