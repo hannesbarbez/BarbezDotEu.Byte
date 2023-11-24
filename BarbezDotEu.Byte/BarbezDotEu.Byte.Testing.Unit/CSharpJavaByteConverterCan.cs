c54b0a287e692bed380d6fececf7032b02042515
Hannes Barbez <hannesbarbez@hotmail.com>
2023-11-24 20:55:47 +0100

    .NET 8

--- BarbezDotEu.Byte/BarbezDotEu.Byte.Testing.Unit/CSharpJavaByteConverterCan.cs
+++ BarbezDotEu.Byte/BarbezDotEu.Byte.Testing.Unit/CSharpJavaByteConverterCan.cs
@@ -8,20 +8,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BarbezDotEu.Byte.Testing.Unit
 {
     [TestClass]
-    public class UnitTests
+    public class CSharpJavaByteConverterCan
     {
-        private const string cSharpByteArrayString = "137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 1, 3, 0, 0, 0, 37, 219, 86, 202, " +
+        private const string CSharpByteArrayString = "137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 0, 1, 0, 0, 0, 1, 1, 3, 0, 0, 0, 37, 219, 86, 202, " +
             "0, 0, 0, 1, 115, 82, 71, 66, 0, 174, 206, 28, 233, 0, 0, 0, 4, 103, 65, 77, 65, 0, 0, 177, 143, 11, 252, 97, 5, 0, 0, 0, 3, 80, 76, 84, 69, 163, 73, 164, " +
             "140, 68, 77, 94, 0, 0, 0, 10, 73, 68, 65, 84, 24, 211, 99, 96, 0, 0, 0, 2, 0, 1, 152, 99, 108, 215, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130";
 
         [TestMethod]
         public void CanBiDirectionallyConvert()
         {
-            var javaByteArray = CSharpJavaByteConverter.ConvertCSharpByteArrayToJavaByteArray(cSharpByteArrayString);
+            var javaByteArray = CSharpJavaByteConverter.ConvertCSharpByteArrayToJavaByteArray(CSharpByteArrayString);
             Assert.IsTrue(javaByteArray.Any());
 
             var cSharpByteArray = CSharpJavaByteConverter.ConvertJavaByteArrayToCSharpByteArray(javaByteArray);
-            Assert.IsTrue(string.Equals(cSharpByteArray, cSharpByteArrayString, StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(string.Equals(cSharpByteArray, CSharpByteArrayString, StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
